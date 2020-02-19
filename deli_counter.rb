d3d6fe44b526948a38c32a69ec9b8a97771cc885
Terry Threatt <terry.threatt@gmail.com>
2020-02-19 03:54:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,15 @@
 require 'pry'
 def line(katz_deli)
   binding.pry 
-  cur_line = []
-  count = 1
   if katz_deli < 1 
      puts "The line is currently empty."
   else 
-    katz_deli.each do |name|{
+    katz_deli.each do |name|
+      cur_line = []
+      count = 1
       cur_line.push("#{count}. #{name}")
-      count += 1}
+      count += 1
+    end 
       puts "The line is currently: #{cur_line.join(" ")}"
   end 
 end
