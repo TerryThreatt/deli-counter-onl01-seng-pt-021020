a0f7def7f21f4143213a4cd9f84b0f54604827b9
Terry Threatt <terry.threatt@gmail.com>
2020-02-19 04:03:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,15 @@
 # Write your code here.
 def line(array)
-  if array < 1 
-     puts "The line is currently empty."
+  if array >= 1
+    cur_line = []
+    count = 1
+    array.each do |name|
+      cur_line << "#{count}. #{name}"
+      count += 1
+    end 
+    puts "The line is currently: #{cur_line.join(" ")}"
   else 
-      cur_line = []
-      count = 1
-      array.each do |name|
-        cur_line << "#{count}. #{name}"
-        count += 1
-      end 
-      puts "The line is currently: #{cur_line.join(" ")}"
+    puts "The line is crrently empty."
   end 
 end
   
