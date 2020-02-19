a1e85f8af41fad4e59b3f4b1df109314d02eb87f
Terry Threatt <terry.threatt@gmail.com>
2020-02-19 03:38:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   if katz_deli >= 1 
     katz_deli.each do |name|
       cur_line.push("#{count}. #{name}")
-      counter += 1
+      count += 1
   end 
       puts "The line is currently: #{cur_line.join(" ")}"
   else 
