db2f85e480085006014d6d0c630b8fa6325940b3
Terry Threatt <terry.threatt@gmail.com>
2020-02-19 03:34:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,8 +7,7 @@ def line(katz_deli)
     katz_deli.each do |name|
       cur_line.push("#{counter}. #{name}")
       counter += 1
-  end 
-    puts "The line is currently: #{cur_line.join(" ")}"
+      puts "The line is currently: #{cur_line.join(" ")}"
   else 
     puts "The line is currently empty."
   end 
