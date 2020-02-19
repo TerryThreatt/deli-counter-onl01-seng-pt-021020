42e362a533c7c9be671b392b37635af949704ac0
Terry Threatt <terry.threatt@gmail.com>
2020-02-19 03:55:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@ def line(katz_deli)
   if katz_deli < 1 
      puts "The line is currently empty."
   else 
-    katz_deli.each do |name|
       cur_line = []
       count = 1
+    katz_deli.each do |name|
       cur_line.push("#{count}. #{name}")
       count += 1
     end 
