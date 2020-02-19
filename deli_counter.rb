1517588b7d0e697c1b39bcbabe5a5883e330ef3a
Terry Threatt <terry.threatt@gmail.com>
2020-02-19 03:19:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def line(katz_deli) 
+  cur_line = []
+  if cur_line == 0 
+    puts "The line is currently empty."
+end
+  
+
+def now_serving(katz_deli)
+  if katz_deli > 
+    katz_deli.shift
+    puts "Currently serving #{katz_deli[0]}"
+end 
