e82a3fce535a98130668eaecc700917b7b2a4e2e
Terry Threatt <terry.threatt@gmail.com>
2020-02-19 03:49:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,13 +3,13 @@
 def line(katz_deli) 
   cur_line = []
   count = 1
-  if katz_deli > 0 
+  if katz_deli < 1 
+     puts "The line is currently empty."
+  else 
     katz_deli.each do |name|
       cur_line.push("#{count}. #{name}")
       count += 1
       puts "The line is currently: #{cur_line.join(" ")}"
-  else 
-    puts "The line is currently empty."
   end 
 end
 
